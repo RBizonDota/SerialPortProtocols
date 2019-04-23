9ac6c545c5d31a1023a158e36175d3752a040383
RBizonDota <rbaybarin@icloud.com>
2019-04-23 19:14:37 +0300

    Added TODO v3

--- hamming.go
+++ hamming.go
@@ -1,16 +1,13 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"math/bits"
 	"os"
 	"strconv"
 	"time"
 )
-
-//TODO не забыть задать нормальные флаги
-var DataTransmitStart = byte(122)
-var DataTransmitEnd = byte(144)
 
 var DataFileName = "text.txt"
 var DataFolder = "data/"
@@ -34,7 +31,7 @@ func ReadFilePart(f *os.File, placeFrom int64, numOfBytes int) []byte {
 	sliceBytes := make([]byte, numOfBytes)
 	_, err = f.Read(sliceBytes)
 	CheckError(err)
-	fmt.Printf("sliceBytes: %s\n", string(sliceBytes))
+	//fmt.Printf("sliceBytes: %s\n", string(sliceBytes))
 	return sliceBytes
 }
 
@@ -45,6 +42,10 @@ func AddFrameType(bytesArr []byte, frameType string) []byte {
 		firstByte = append(firstByte, 240)
 	case "noinfo":
 		firstByte = append(firstByte, 42) // еще кейсы
+	case "init":
+		firstByte = append(firstByte, 122)
+	case "end":
+		firstByte = append(firstByte, 144)
 	default:
 		firstByte = append(firstByte, 42)
 	}
@@ -55,9 +56,9 @@ func AddFrameType(bytesArr []byte, frameType string) []byte {
 
 func ToBits(sliceBytes []byte) int64 {
 	var bits int64
-	sliceBytes = AddFrameType(sliceBytes, "info")
-	fmt.Println(sliceBytes)
-
+	//sliceBytes = AddFrameType(sliceBytes, "info")
+	//fmt.Println(sliceBytes)
+	//TODO сделать отслеживание размера кадров (если пришло меньше 6, то дополнить 0-ми байтами)
 	for _, b := range sliceBytes {
 		bits <<= 8
 		bits += int64(b)
@@ -111,7 +112,7 @@ func Decode(msg int64, msgLen int) ([]byte, bool) {
 		syndr |= syndrBit
 		position *= 2
 	}
-	fmt.Println("syndr: ", syndr)
+	//fmt.Println("syndr: ", syndr)
 
 	if syndr != 0 {
 		//исправить 1ую ошибку
@@ -141,15 +142,15 @@ func DeleteControlBits(msg int64) int64 {
 func ToBytes(msg int64) []byte {
 	var curBit int64
 	var curByte byte
-	var frameType byte
+	//var frameType byte
 	var sliceBytes []byte
 	var mask int64 = 0x7F80000000000000
 	msg <<= 7
 
 	//получить тип кадра
-	frameType = byte((msg & mask) >> 55)
+	//frameType = byte((msg & mask) >> 55)
 	msg <<= 8
-	fmt.Printf("frameType: %d\n", frameType)
+	//fmt.Printf("frameType: %d\n", frameType)
 
 	for len(sliceBytes) != 6 {
 		curBit = msg & mask
@@ -159,7 +160,7 @@ func ToBytes(msg int64) []byte {
 		msg <<= 8
 	}
 
-	fmt.Println("sliceBytes: ", sliceBytes)
+	//fmt.Println("sliceBytes: ", sliceBytes)
 	return sliceBytes
 }
 
@@ -174,8 +175,10 @@ func main() {
 	//TODO Отправка флага начала передачи, длины названия в кадрах, длины тела в кадрах
 	//TODO (кадр флаг 1-м байтом, два последних - длина названия и длина тела)
 	//--------------Отправка файла----------------
+	start := time.Now()
 	var fileSize, nameSize, i, bytesToRead int64
-	var fileCadrSize, nameCadrSize byte
+	var nameCadrSize int16
+	var fileCadrSize int16
 	file, err := os.Open(DataFileName)
 	CheckError(err)
 
@@ -185,24 +188,41 @@ func main() {
 	nameSize = int64(len(DataFileName))
 	fmt.Printf("FileSize: %d\n", fileSize)
 	bytesToRead = 6
-	fileCadrSize = byte(fileSize/bytesToRead + 1)
-	nameCadrSize = byte(nameSize/bytesToRead + 1)
+	fileCadrSize = int16(fileSize/bytesToRead + 1)
+	nameCadrSize = int16(nameSize/bytesToRead + 1)
 	fmt.Println("fileSize = " + strconv.Itoa(int(fileSize)) + ", fileCadrSize = " + strconv.Itoa(int(fileCadrSize)))
 	fmt.Println("nameSize = " + strconv.Itoa(int(nameSize)) + ", nameCadrSize = " + strconv.Itoa(int(nameCadrSize)))
-	//TODO сбор кадра начала передачи данные в переменных fileCadrSize и nameCadrSize
-	//TODO цикл передачи названия
+	//----------------------Инициализирующее сообщение---------------------------
+	initMsg := GetInitMsg(fileCadrSize, nameCadrSize)
+	dataInBits := ToBits(initMsg)
+	mychan <- Code(dataInBits, bits.Len(uint(dataInBits)))
+	//---------------------------------------------------------------------------
+	//----------------------Передача названия------------------------------------
+	nameBytes := []byte(DataFileName)
+	for len(nameBytes)%int(bytesToRead) != 0 { //TODO переписать, неэффективно
+		nameBytes = append(nameBytes, 0)
+	}
+	for i = 0; i < nameSize; i += bytesToRead {
+		//TODO цикл передачи названия
+		nameSlice := AddFrameType(nameBytes[i:i+bytesToRead], "info")
+		dataInBits := ToBits(nameSlice)
+		mychan <- Code(dataInBits, bits.Len(uint(dataInBits)))
+	}
+	//----------------------------------------------------------
 	for i = 0; i < fileSize; i += bytesToRead {
 		sliceOfBytes := ReadFilePart(file, i, int(bytesToRead))
+		sliceOfBytes = AddFrameType(sliceOfBytes, "info")
 		dataInBits := ToBits(sliceOfBytes)
-		fmt.Printf("dataBits: %064b\n", dataInBits)
-		fmt.Printf("dataBitsLen: %d\n", bits.Len(uint(dataInBits)))
+		//fmt.Printf("dataBits: %064b\n", dataInBits)
+		//fmt.Printf("dataBitsLen: %d\n", bits.Len(uint(dataInBits)))
 		codedMsg := Code(dataInBits, bits.Len(uint(dataInBits)))
-		fmt.Printf("codedMsg: %064b\n\n", codedMsg)
+		//fmt.Printf("codedMsg: %064b\n\n", codedMsg)
 		mychan <- codedMsg
 	}
 	//TODO Отправка флага конца передачи
 	close(mychan)
 	time.Sleep(time.Second)
+	fmt.Println(time.Since(start))
 
 	//---------------Получение файла------------------------
 	// var bytesArr []byte
@@ -218,23 +238,29 @@ func main() {
 }
 
 func Getter(mychan chan int64) {
-	tmpArr := make([]byte, 0, 1)
-	for {
-		//TODO Обработка не закрытия канала, а флага начала и конца передачи
-		msg, val := <-mychan
-		if !val {
-			break
-		}
-		decoded, valid := Decode(msg, bits.Len(uint(56)))
-		fmt.Printf("decoded:%08b, valid:%t\n\n", decoded, valid)
-		tmpArr = append(tmpArr, decoded...)
-
+	//tmpArr := make([]byte, 0, 1)
+	//TODO Обработка не закрытия канала, а флага начала и конца передачи
+	msg, val := <-mychan
+	if !val {
+		fmt.Println("ERROR!!! chanal closed")
 	}
+	//TODO архитектура не предусматривает анализ типа кадра
+	//TODO ЭТО ОЧЕНЬ ПЛОХО
+	decoded, _ := Decode(msg, bits.Len(uint(56)))
+	fileSizeSlice := decoded[0:2]
+	nameSizeSlice := decoded[2:4]
+	fileSize := binary.LittleEndian.Uint16(fileSizeSlice)
+	nameSize := binary.LittleEndian.Uint16(nameSizeSlice)
+	fmt.Println(fileSize)
+	fmt.Println(nameSize)
+	//fmt.Printf("decoded:%08b, valid:%t\n\n", decoded, valid)
+	//tmpArr = append(tmpArr, decoded...)
+
 	//TODO Сохранение файла в папку /data
 	//TODO сделать /data - выносной переменной DataFolder
-	fmt.Println(tmpArr)
-	fmt.Printf("Text: %s\n\n", string(tmpArr))
-	DataToFile("Test.txt", tmpArr)
+	//fmt.Println(tmpArr)
+	//fmt.Printf("Text: %s\n\n", string(tmpArr))
+	//DataToFile("Test.txt", tmpArr)
 }
 
 //DataToFile true - все хорошо,false - возникла ошибка; ТРЕБУЕТ СУЩЕСТВОВАНИЯ ДИРЕКТОРИИ!!!
@@ -248,4 +274,18 @@ func DataToFile(filename string, body []byte) bool {
 	defer file.Close()
 	file.Write(body)
 	return true
+}
+
+func GetInitMsg(fileCadrSize int16, nameCadrSize int16) []byte {
+	initMsgBytes1 := make([]byte, 2) //fileCadrSize
+	initMsgBytes2 := make([]byte, 2) //nameCadrSize
+	initMsgBytes3 := make([]byte, 2) //StartCadr (для восстановления после разрыва)
+	//WARNING МЕНЯЕТ БАЙТЫ МЕСТАМИ, СНАЧАЛА МЛАДШИЕ, ПОТОМ СТАРШИЕ
+	binary.LittleEndian.PutUint16(initMsgBytes1, uint16(fileCadrSize))
+	binary.LittleEndian.PutUint16(initMsgBytes2, uint16(nameCadrSize))
+	resinitMsg := append(initMsgBytes1, initMsgBytes2...)
+	resinitMsg = append(resinitMsg, initMsgBytes3...)
+	resinitMsg = AddFrameType(resinitMsg, "init")
+	fmt.Println(resinitMsg)
+	return resinitMsg
 }
