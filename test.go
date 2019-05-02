ab61047cb3ea9010dff3199d60e4a11e97717ae7
RBizonDota <rbaybarin@icloud.com>
2019-05-02 19:04:06 +0300

    Changed test.go

--- test.go
+++ test.go
@@ -6,7 +6,7 @@ import (
 	"net"
 )
 
-func testHandleConnection(conn net.Conn) {
+func testHandleConnection(conn net.Conn, prefix string) {
 	name := conn.RemoteAddr().String()
 
 	fmt.Printf("%+v connected\n", name)
@@ -19,12 +19,25 @@ func testHandleConnection(conn net.Conn) {
 		text := scanner.Text()
 		if text == "Exit" {
 			conn.Write([]byte("Bye\n\r"))
-			fmt.Println(name, "disconnected")
+			fmt.Println(prefix, name, "disconnected")
 			break
 		} else if text != "" {
-			fmt.Println(name, "enters", text)
+			fmt.Println(prefix, name, "enters", text)
 			//conn.Write([]byte("You enter " + text + "\n\r"))
 		}
+	}
+}
+func slaveListener() {
+	listner, err := net.Listen("tcp", ":8889")
+	if err != nil {
+		panic(err)
+	}
+	for {
+		conn, err := listner.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go testHandleConnection(conn, "SLAVE ")
 	}
 }
 
@@ -38,6 +51,6 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		go testHandleConnection(conn)
+		go testHandleConnection(conn, "MASTER")
 	}
 }
